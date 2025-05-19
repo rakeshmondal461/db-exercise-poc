0f9cc8a60895b5a850edd23105ab90bdc12c74b7
Rakesh <mondalrakesh461@gmail.com>
2025-05-20 00:01:01 +0530

    first and last used

--- src/pipelines/products.ts
+++ src/pipelines/products.ts
@@ -1,9 +1,17 @@
 export const accoperatorPipeline = [
   {
+    $sort: { price: -1 },
+  },
+  {
     $group: {
       _id: "$category",
-      cheapest: { $min: "$price" },
-      mostexpensive: { $max: "$price" },
+      // Minimum and Maximum 
+      //   cheapest: { $min: "$price" },
+      //   mostexpensive: { $max: "$price" },
+
+      // Returns the first or last value in a group
+      first: { $first: "$price" },
+      last: { $last: "$price" },
     },
   },
 ];
